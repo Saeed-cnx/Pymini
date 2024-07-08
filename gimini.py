ca36ba9f99a217aff130db3ce2b7781147cf8c15
Saeed-cnx <workifebalance.cnx@gmail.com>
2024-07-08 09:36:33 +0700

    fix indent

--- gimini.py
+++ gimini.py
@@ -2,15 +2,15 @@ import streamlit as st
 import google.generativeai as genai
 
 genai.configure(api_key="AIzaSyCdVsOk8b7qrXcAYsQ9UXgyDJL2cEGJW6c")
-model = genai.GenerativeModel(“gemini-pro”)
-st.title(“แปลภาษา”)
-ch = st.selectbox(“เลือกภาษาปลายทาง”,(“ไทย”,”อังกฤษ”,”เกาหลี”,”ญี่ปุ่น”))
-text_in = st.text_input(“ป้อนข้อความที่ต้องการแปล: “)
-prompt = “แปลข้อความต่อไปนี้เป็นภาษา”+ ch + ” ” + text_in
+model = genai.GenerativeModel("gemini-pro")
+st.title("แปลภาษา")
+ch = st.selectbox("เลือกภาษาปลายทาง",("ไทย","อังกฤษ","เกาหลี","ญี่ปุ่น"))
+text_in = st.text_input("ป้อนข้อความที่ต้องการแปล: ")
+prompt = "แปลข้อความต่อไปนี้เป็นภาษา"+ ch + " " + text_in
 st.text(prompt)
-if st.button(“แปล”):
-try:
-response = model.generate_content(prompt)
-st.text(response.text)
-except:
-st.text(“no response”)
+if st.button("แปล"):
+    try:
+        response = model.generate_content(prompt)
+        st.text(response.text)
+    except:
+        st.text("no response")
